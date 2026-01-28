379122a78353ae2b27b13a1e896af4e5bb0450b3
Phinehas Lamptey <lampteyphinehas70@gmail.com>
2026-01-28 17:07:20 +0000

    configurations done

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => AdminAuth::class,
 
         ]) ;
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
